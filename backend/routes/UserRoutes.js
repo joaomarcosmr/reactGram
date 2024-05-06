26e6e2d237bf2131fec9e3d50d4fb0a8fba1899a
João Marcos <joaomarcosmr4@gmail.com>
2024-05-06 18:01:10 -0300

    estudos

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,8 +3,13 @@ const router = express.Router()
  
 // Controller
 const { register } = require("../controllers/UserController")
- 
+
+
+//Middlewares
+const validate = require('../middlewares/handleValidation')
+const { userCreateValidation } = require('../middlewares/userValidation')
+
 // Routes
-router.post("/register", register)
+router.post("/register", userCreateValidation(), validate, register)
  
 module.exports = router
